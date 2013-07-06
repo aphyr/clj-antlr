85ec2a06a6fea717eaabe8563f763b2a82f246ce
Aphyr <aphyr@aphyr.com>
2013-07-06 13:06:44 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-antlr "0.0.1-SNAPSHOT"
+(defproject clj-antlr "0.1.0"
   :description "Clojure sugar over the ANTLR 4 parser library."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.antlr/antlr4-runtime "4.0"]]
   :profiles {:dev {:dependencies
                    [[midje "1.5.0"]
