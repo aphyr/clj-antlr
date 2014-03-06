7c3d532c38e18323ddc3a3c1bb2bedad82e362ee
Kyle Kingsbury <aphyr@aphyr.com>
2014-03-05 16:52:21 -0800

    0.2.0

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject clj-antlr "0.1.1-SNAPSHOT"
-  :description "Clojure sugar over the ANTLR 4 parser library."
-  :url "http://example.com/FIXME"
+(defproject clj-antlr "0.2.0"
+  :description "Clojure bindings for the ANTLR 4 parser library."
+  :url "http://github.com/aphyr/clj-antlr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.antlr/antlr4-runtime "4.2"]
                  [org.antlr/antlr4 "4.2"]]
   :profiles {:dev {:dependencies
-                  [[criterium "0.4.0"]
+                   [[criterium "0.4.0"]
                     [instaparse "1.2.6"]]}}
   :java-source-paths ["src/java/"]
   :test-selectors {:default #(not (:perf %))
