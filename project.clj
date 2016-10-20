a9543271728b17a06f7c8cf44eb0e317541328c8
Aphyr <aphyr@aphyr.com>
2016-10-20 11:12:27 -0500

    0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-antlr "0.2.3"
+(defproject clj-antlr "0.2.4"
   :description "Clojure bindings for the ANTLR 4 parser library."
   :url "http://github.com/aphyr/clj-antlr"
   :license {:name "Eclipse Public License"
