d5fc45d57170c0d10880272b36f6c293c2b89517
Aphyr <aphyr@aphyr.com>
2021-09-06 10:03:13 -0400

    0.2.10: bump Clojure, Cheshire, slf4j

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject clj-antlr "0.2.10-SNAPSHOT"
+(defproject clj-antlr "0.2.10"
   :description "Clojure bindings for the ANTLR 4 parser library."
   :url "http://github.com/aphyr/clj-antlr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.antlr/antlr4-runtime "4.9.2"]
                  [org.antlr/antlr4 "4.9.2"]
                  [org.clojure/tools.logging "1.1.0"]]
   :profiles {:dev {:dependencies
                    [[criterium "0.4.6"]
-                    [cheshire "5.10.0"]
+                    [cheshire "5.10.1"]
                     [org.clojure/test.check "1.1.0"]
-                    [org.slf4j/slf4j-simple "1.7.30"]
+                    [org.slf4j/slf4j-simple "1.7.32"]
                     [instaparse "1.4.10"]]}}
   :java-source-paths ["src/java/"]
   :javac-options ["-target" "1.8" "-source" "1.8"]
